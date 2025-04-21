97e180f8499c365a9e77723fc39956c5ca17887c
Daniele <112819513+DanieleSetti@users.noreply.github.com>
2025-04-21 14:24:44 +0200

    Delete update_system.sh

--- update_system.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Update system script
-LOGFILE="/var/log/update_system.log"
-echo "==== $(date '+%Y-%m-%d %H:%M:%S') ====" >> "$LOGFILE"
-apt update && apt upgrade -y >> "$LOGFILE" 2>&1
-echo "Update finished." >> "$LOGFILE"
